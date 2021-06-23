b588956f9a5d71b79d88521e45000112397993fb
Shiva <isha@abcd.com>
2021-06-23 21:52:35 +0530

    classes

--- jscript/importrxport/index.js
+++ jscript/importrxport/index.js
@@ -4,5 +4,6 @@ import {add,EC2_URL} from './MathFunction.js';
 import MyStringClass from './MyAnotherClass.js';
 
 
+import {yahoomaps} from '@yahoo/maps';
 
-export {add,EC2_URL,MyStringClass}
+export {add,EC2_URL,MyStringClass,yahoomaps as gpsprov }
